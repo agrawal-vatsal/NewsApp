5d92bc740197493586f674a9ba1d80de70945a2a
Vatsal <vtslaggarwal@gmail.com>
2018-07-20 01:30:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vatsal/newsly/Activities/LoggedInActivity.java
@@ -0,0 +1,75 @@
+package com.example.vatsal.newsly.Activities;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.example.vatsal.newsly.Adapters.ViewPagerAdapter;
+import com.example.vatsal.newsly.R;
+import com.facebook.login.LoginManager;
+
+
+public class LoggedInActivity extends AppCompatActivity {
+    boolean isGoogleAccount;
+    Toolbar toolbar;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        Intent intent = getIntent();
+        isGoogleAccount = intent.getExtras().getBoolean(SignInActivity.isGoogleAccount);
+
+
+        viewPager = findViewById(R.id.viewpager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+
+        tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sign_out_menu:
+                if (isGoogleAccount)
+                    SignInActivity.mGoogleSignInClient.signOut();
+                else
+                    LoginManager.getInstance().logOut();
+                Intent intent = new Intent(LoggedInActivity.this, SignInActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/com/example/vatsal/newsly/Activities/WebPageActivity.java
@@ -0,0 +1,23 @@
+package com.example.vatsal.newsly.Activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import com.example.vatsal.newsly.R;
+
+public class WebPageActivity extends AppCompatActivity {
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_page);
+        webView = findViewById(R.id.webView);
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        String url = bundle.getString("webPage");
+        webView.loadUrl(url);
+    }
+}
